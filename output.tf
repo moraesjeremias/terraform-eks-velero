aaff6d61e329a4263c67f30d7c26763c97f1763a
Jeremias Moraes <moraesjeremias@gmail.com>
2021-08-24 12:51:55 -0300

    main - Enable provisioning outputs

---
+++ output.tf
@@ -0,0 +1,10 @@
+output "kubernetes_cluster_name" {
+  value       = google_container_cluster.velero_backup.name
+  description = "GKE Cluster Name"
+  sensitive   = true
+}
+
+output "kubernetes_cluster_host" {
+  value       = google_container_cluster.velero_backup.endpoint
+  description = "GKE Cluster Host"
+}
