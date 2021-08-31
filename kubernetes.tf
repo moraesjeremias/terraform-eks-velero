5bafc0eaf4d3b3d35ff97b4d426a78d755f473ae
Jeremias Moraes <jeremias.moraes@creditas.com>
2021-08-31 15:39:20 -0300

    main - creating single zonal cluster + storage auth_scope

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,25 +1,42 @@
 # GKE Backup Cluster
 resource "google_container_cluster" "velero_backup" {
   name                     = "velero-backup"
-  location                 = var.region
+  location                 = var.zone
   remove_default_node_pool = true
   initial_node_count       = 1
 
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
+
+  cluster_autoscaling {
+    enabled = true
+    resource_limits {
+      resource_type = "memory"
+      minimum       = 4096
+      maximum       = 6144
+    }
+    resource_limits {
+      resource_type = "cpu"
+      minimum       = 1
+      maximum       = 4
+    }
+  }
 }
 
 # Separately Managed Node Pool
 resource "google_container_node_pool" "velero_backup_nodes" {
   name       = "${google_container_cluster.velero_backup.name}-node-pool"
-  location   = var.region
+  location   = var.zone
   cluster    = google_container_cluster.velero_backup.name
   node_count = var.gke_num_nodes
 
   node_config {
+    preemptible = true
+    service_account = var.service_account_email
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/devstorage.full_control",
     ]
 
     labels = {
@@ -27,7 +44,7 @@ resource "google_container_node_pool" "velero_backup_nodes" {
     }
 
     # preemptible  = true
-    machine_type = "n1-standard-1"
+    machine_type = "e2-custom-4-6144"
     tags         = ["gke-node", "${var.gcp_project}-gke", google_container_cluster.velero_backup.name]
     metadata = {
       disable-legacy-endpoints = "true"
@@ -38,25 +55,42 @@ resource "google_container_node_pool" "velero_backup_nodes" {
 # GKE Recovery Cluster
 resource "google_container_cluster" "velero_recovery" {
   name                     = "velero-recovery"
-  location                 = var.alt_region
+  location                 = var.zone
   remove_default_node_pool = true
   initial_node_count       = 1
 
   network    = google_compute_network.recovery_vpc.name
   subnetwork = google_compute_subnetwork.recovery_subnet.name
+
+  cluster_autoscaling {
+    enabled = true
+    resource_limits {
+      resource_type = "memory"
+      minimum       = 4096
+      maximum       = 6144
+    }
+    resource_limits {
+      resource_type = "cpu"
+      minimum       = 1
+      maximum       = 4
+    }
+  }
 }
 
 # Separately Managed Node Pool
 resource "google_container_node_pool" "velero_recovery_nodes" {
   name       = "${google_container_cluster.velero_recovery.name}-node-pool"
-  location   = var.alt_region
+  location   = var.zone
   cluster    = google_container_cluster.velero_recovery.name
   node_count = var.gke_num_nodes
 
   node_config {
+    preemptible = true
+    service_account = var.service_account_email
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/devstorage.full_control",
     ]
 
     labels = {
@@ -64,7 +98,7 @@ resource "google_container_node_pool" "velero_recovery_nodes" {
     }
 
     # preemptible  = true
-    machine_type = "n1-standard-1"
+    machine_type = "e2-custom-4-6144"
     tags         = ["gke-node", "${var.gcp_project}-gke", google_container_cluster.velero_recovery.name]
     metadata = {
       disable-legacy-endpoints = "true"
